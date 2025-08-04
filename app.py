868f69f169b27a07b11abeb9df0ebe892e772176
Aadithya1826 <143221419+Aadithya1826@users.noreply.github.com>
2025-08-04 22:46:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,5 +140,7 @@ def upload_and_process():
 
 # === Run on Railway ===
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    import os
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
+
